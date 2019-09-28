b50b902eab8859cc982e909ae1c5721f7135ab88
cjhines <chrishinesabdn@gmail.com>
2019-09-28 12:45:30 +0200

    Updates

---
+++ precache-manifest.b98004f80f7f72d4b46138c9a7917fd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33b55ff4391217ad3650a36330b06ca7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b563002170fd035c27af",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "1bee6e3f452e5969aed2",
+    "url": "/static/js/2.34550376.chunk.js"
+  },
+  {
+    "revision": "b563002170fd035c27af",
+    "url": "/static/js/main.6b35e26a.chunk.js"
+  },
+  {
+    "revision": "4bf5d4777bc7c135da6e",
+    "url": "/static/js/runtime-main.e3206f37.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
