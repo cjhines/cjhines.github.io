76de3a26ef047c8d2fa8e844aadc75dce91e48fb
cjhines <christopher.hines@hybridheroes.de>
2021-09-04 09:39:07 +0200

    chore: update biography

--- src/components/Biography.tsx
+++ src/components/Biography.tsx
@@ -24,7 +24,7 @@ const Biography: React.FunctionComponent = () => (
       </h1>
       <br />
       <h1>
-        {'I\'m always open for collaborations or interesting ventures. Feel free to '}
+        {'I\'m always open to collaborations or interesting ventures. Feel free to '}
         <a href={CV} rel="noopener noreferrer" target="_blank">
           view my CV
         </a>
