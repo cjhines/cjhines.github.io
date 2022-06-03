24dd09d46e8977a2ba8b81669bd6e165f0f0338a
cjhines <christopher.hines@hybridheroes.de>
2022-06-03 14:01:16 +0200

    docs: comments

--- src/geometry/Cell.ts
+++ src/geometry/Cell.ts
@@ -1,6 +1,6 @@
 import Circle from './Circle';
 
-// Configuration objects for each circle within the cell
+// Configuration objects for each circle within the cell.
 const configurations = [
   {
     colour: '#FFC107',
@@ -28,16 +28,16 @@ const configurations = [
   },
 ];
 
-// Cell class which consists of a number of pre-configured circles
+// Cell class which consists of a number of pre-configured circles.
 export default class {
   circles: Circle[];
 
-  // Initialise with canvas and dimensions
+  // Initialise with canvas and dimensions.
   constructor(canvas: CanvasRenderingContext2D, width: number, height: number) {
     const x = width / 2;
     const y = height / 2;
 
-    // Initialise circles from configuration array entries
+    // Initialise circles from configuration array entries.
     this.circles = [];
     configurations.forEach((configuration) => {
       this.circles.push(
@@ -45,14 +45,14 @@ export default class {
           x,
           y,
           canvas,
-          compositionOperation: 'source-over', // default
+          compositionOperation: 'source-over', // Default.
           ...configuration,
         })
       );
     });
   }
 
-  // Iterate through configured circles and animate
+  // Iterate through configured circles and animate.
   animate = (): void => {
     this.circles.forEach((circle) => circle.draw());
   };
