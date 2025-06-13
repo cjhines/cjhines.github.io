824ae6aaf28f4f840a1ece472cf69c643b7e9239
cjhines <chrishinesabdn@gmail.com>
2025-06-13 11:13:03 +0200

    move home page component

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import Biography from '../components/Biography';
-import Canvas from '../components/Canvas';
-import SocialBar from '../components/SocialBar';
+import Biography from '../../components/Biography';
+import Canvas from '../../components/Canvas';
+import SocialBar from '../../components/SocialBar';
 
 const HomePage: React.FunctionComponent = () => (
   <>
