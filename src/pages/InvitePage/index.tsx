2b041ce2b2cfa6537c4dbe12258b5f2af7359e24
cjhines <chrishinesabdn@gmail.com>
2025-06-13 14:23:19 +0200

    add more elements

--- src/pages/InvitePage/index.tsx
+++ src/pages/InvitePage/index.tsx
@@ -3,6 +3,8 @@ import floralPattern from './assets/floralPattern.svg';
 import wavePattern from './assets/wavePattern.svg';
 import matatu from './assets/matatu.png';
 import matatu2 from './assets/matatu2.png';
+import baobub from './assets/baobub.png';
+import kaos from './assets/kaos.png';
 import portrait from './assets/portrait.png';
 import styles from './InvitePage.module.css';
 
@@ -23,6 +25,10 @@ const Invite: React.FunctionComponent = () => (
       <div className={styles.matatuContainer}>
         <img src={matatu} alt="matatu" className={styles.matatuImage} />
         <img src={matatu2} alt="matatu2" className={styles.matatuImage2} />
+      </div>
+      <div className={styles.footerContainer}>
+        <img src={kaos} alt="kaos" className={styles.footerImage} />
+        <img src={baobub} alt="baobab" className={styles.footerImage} />
       </div>
     </div>
   </div>
