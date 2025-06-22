be4cf5dd587f2f20997c78a628d7efc137496c5e
cjhines <chrishinesabdn@gmail.com>
2025-06-22 18:33:43 +0200

    chore: adjust router

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import HomePage from './pages/HomePage';
 import InvitePage from './pages/InvitePage';
 import './App.scss';
 
 const App: React.FunctionComponent = () => (
-  <HashRouter>
+  <BrowserRouter>
     <div className="app">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/tschuess" element={<InvitePage />} />
       </Routes>
     </div>
-  </HashRouter>
+  </BrowserRouter>
 );
 
 export default App;
